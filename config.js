cf84316052a7ea5995616a39bc4518eec47e063f
FAMOUSANAS <choudharyanasmehar@gmail.com>
2024-11-27 11:10:36 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Xzxh3ZQC#A3WBJ7mjbfefjm8zOo93GJ-XPnKQAJMPIUBRVaMNaKY",
   // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
   // add your prifix for bot
