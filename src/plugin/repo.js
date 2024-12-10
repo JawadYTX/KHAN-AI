cb369c0999c4c22c68bad72f924e55ad83caae50
Jawad Tech <jawadofcx@gmail.com>
2024-12-10 20:23:15 +0500

    Update repo.js

--- src/plugin/repo.js
+++ src/plugin/repo.js
@@ -3,76 +3,44 @@ const { generateWAMessageFromContent, proto } = pkg;
 import axios from 'axios'; // Import axios for HTTP requests
 
 const handleRepoCommand = async (m, Matrix) => {
-  const repoUrl = 'https://api.github.com/repos/JawadTechX/KHAN-AI';
+  const repoUrl = 'https://api.github.com/repos/Ethix-Xsid/Ethix-MD-V2';
   try {
     const response = await axios.get(repoUrl);
     const repoData = response.data;
 
     const { full_name, name, forks_count, stargazers_count, created_at, updated_at, owner } = repoData;
 
-    const messageText = `Hello *_${m.pushName}_,*\nThis is *KHAN-AI,* A Whatsapp Bot Built by *JawadTechX,* Enhanced with Amazing Features to Make Your Whatsapp Communication and Interaction Experience Amazing\n\n*❲❒❳ ɴᴀᴍᴇ:* ${name}\n*❲❒❳ sᴛᴀʀs:* ${stargazers_count}\n*❲❒❳ ғᴏʀᴋs:* ${forks_count}\n*❲❒❳ ᴄʀᴇᴀᴛᴇᴅ ᴏɴ:* ${new Date(created_at).toLocaleDateString()}\n*❲❒❳ ʟᴀsᴛ ᴜᴘᴅᴀᴛᴇᴅ:* ${new Date(updated_at).toLocaleDateString()}`;
+    const messageText = `Hello *_${m.pushName}_,*\n\nThis is *Ethix-MD,* a WhatsApp bot built by *Ethix-Xsid,* enhanced with amazing features to make your WhatsApp communication experience amazing!\n\n*❲❒❳ ɴᴀᴍᴇ:* ${name}\n*❲❒❳ sᴛᴀʀs:* ${stargazers_count}\n*❲❒❳ ғᴏʀᴋs:* ${forks_count}\n*❲❒❳ ᴄʀᴇᴀᴛᴇᴅ ᴏɴ:* ${new Date(created_at).toLocaleDateString()}\n*❲❒❳ ʟᴀsᴛ ᴜᴘᴅᴀᴛᴇᴅ:* ${new Date(updated_at).toLocaleDateString()}\n\n🔗 *OWNER:* [Click here](https://wa.me/owner_number_here)\n🔗 *FOLLOW WACHANNEL:* [Click here](https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S)\n🔗 *FORK REPO:* [Click here](https://github.com/Ethix-Xsid/Ethix-MD-V2/fork)\n\n*© Powered by Ethix-MD*`;
 
     const repoMessage = generateWAMessageFromContent(m.from, {
       viewOnceMessage: {
         message: {
           messageContextInfo: {
             deviceListMetadata: {},
-            deviceListMetadataVersion: 2
+            deviceListMetadataVersion: 2,
           },
           interactiveMessage: proto.Message.InteractiveMessage.create({
             body: proto.Message.InteractiveMessage.Body.create({
-              text: messageText
-            }),
-            footer: proto.Message.InteractiveMessage.Footer.create({
-              text: "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʜᴀɴ-ᴍᴅ*"
+              text: messageText,
             }),
             header: proto.Message.InteractiveMessage.Header.create({
-             ...(await prepareWAMessageMedia({ image: { url: `https://raw.githubusercontent.com/JawadTechX/KHAN-AI/main/src/khan.jpg` } }, { upload: Matrix.waUploadToServer })),
+              ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/fbbe1744668b44637c21a.jpg` } }, { upload: Matrix.waUploadToServer })),
               title: "",
-              gifPlayback: true,
-              subtitle: "",
-              hasMediaAttachment: false 
-            }),
-            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-              buttons: [
-                {
-                  name: "quick_reply",
-                  buttonParamsJson: JSON.stringify({
-                    display_text: "OWNER",
-                    id: ".owner"
-                  })
-                },
-                {
-                  name: "cta_url",
-                  buttonParamsJson: JSON.stringify({
-                    display_text: "GITHUB REPOSITORY",
-                    url: `https://github.com/JawadTechX/KHAN-AI`
-                  })
-                },
-                {
-                  name: "cta_url",
-                  buttonParamsJson: JSON.stringify({
-                    display_text: "CLOCK HERE TO FORK",
-                    url: `https://github.com/JawadTechX/KHAN-AI/fork`
-                  })
-                }
-              ],
             }),
             contextInfo: {
               mentionedJid: [m.sender],
               forwardingScore: 9999,
               isForwarded: false,
-            }
+            },
           }),
         },
       },
     }, {});
 
     await Matrix.relayMessage(repoMessage.key.remoteJid, repoMessage.message, {
-      messageId: repoMessage.key.id
+      messageId: repoMessage.key.id,
     });
     await m.React("✅");
-
   } catch (error) {
     console.error("Error processing your request:", error);
     m.reply('Error processing your request.');
@@ -81,8 +49,7 @@ const handleRepoCommand = async (m, Matrix) => {
 };
 
 const searchRepo = async (m, Matrix) => {
-  // const prefixMatch = m.body.match(/^[\\/!#.]/);
-   const prefixMatch = m.body.match(/^[+×÷=/_<>[\]!@#.£%^&*()\-"'1234567890?,°€£^:;?¿‽】〕」』【〔「『<>_${}\|`《○♡○¡☆《●●■◇¡¤▪︎•°~♡●♧₩$€○》☆¡Abcdefghijklmonpqrstuvwxyz]/i); 
+  const prefixMatch = m.body.match(/^[+×÷=/_<>[\]!@#.£%^&*()\-"'1234567890?,°€£^:;?¿‽】〕」』【〔「『<>_${}\|`《○♡○¡☆《●●■◇¡¤▪︎•°~♡●♧₩$€○》☆¡Abcdefghijklmonpqrstuvwxyz]/i); 
   const prefix = prefixMatch ? prefixMatch[0] : '/';
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
